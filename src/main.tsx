0c29870c182a11fff2b2dece9ecb8ef145ce83be
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2025-10-29 12:03:19 +0100

    react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import { lazy } from 'react';
+
+const About = lazy(() => import('./pages/about-us/index.tsx'))
+
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter basename="/sport-hub">
+     <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>,
 )
